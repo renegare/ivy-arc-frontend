6e8e504eb67ab66f5a0952d65d85d2442a0ab1b6
Mudi <mudi@renegare.com>
2020-12-16 19:08:19 +0000

    removed uncessary code

--- src/App.js
+++ src/App.js
@@ -5,21 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Ivy Arc Frontend
+          Real life project!!!
         </p>
-        <p>
-          CI / CD Surgery
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Programming!
-        </a>
+
       </header>
     </div>
   );
